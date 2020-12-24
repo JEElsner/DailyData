3d7f9f664eb2dfcbcd8aca789029594ef5af3c6d
Jonathan Elsner <jelsnerbusiness@outlook.com>
2020-12-23 21:17:35 -0800

    Create setup.py stub

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+# Stub for a package setup file. This needs to be completed to make a package
+# out of this project.
+#
+# Read these resources to finish it dunderhead:
+# https://packaging.python.org/tutorials/packaging-projects/
+# https://packaging.python.org/key_projects/#setuptools
+# https://github.com/pypa/sampleproject/blob/master/setup.pys
+
+setup(
+    package_dir={'': 'src'},  # Specify that the package is in the 'src' folder
+)
