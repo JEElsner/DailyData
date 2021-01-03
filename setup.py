4541f466354e938c58903b0f208834bd7572ba85
Jonathan Elsner <jelsnerbusiness@outlook.com>
2021-01-02 20:22:22 -0800

    Update setup and README

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 from setuptools import setup, find_packages
 
-# Stub for a package setup file. This needs to be completed to make a package
-# out of this project.
-#
-# Read these resources to finish it dunderhead:
-# https://packaging.python.org/tutorials/packaging-projects/
-# https://packaging.python.org/key_projects/#setuptools
-# https://github.com/pypa/sampleproject/blob/master/setup.pys
+with open('README.md', 'r', encoding='utf-8') as readme:
+    long_description = readme.read()
 
 setup(
     name='DailyData',
+    version='0.0.1',
+    author='Jonathan Elsner',
+    author_email='jeelsner@outlook.com',
+    description='A package for recording and reviewing data about your day',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/JEElsner/DailyData/',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},  # Specify that the package is in the 'src' folder
-    install_requires=['ConsoleQuestionPrompts', 'python-docx'],
+    install_requires=['ConsoleQuestionPrompts',
+                      'python-docx', 'pandas', 'numpy'],
     entry_points={
         'console_scripts': [
             'timelog=DailyData.time_management:timelog_entry_point',
             'dailydata=DailyData:take_args'
         ]
     },
-    include_package_data=True
+    include_package_data=True,
+    python_requires='>=3.6'
 )
