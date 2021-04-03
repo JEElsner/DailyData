d75fd4f11a9d1a8235a963c9ebf418d77da081e9
Jonathan Elsner <jelsnerbusiness@outlook.com>
2021-04-02 22:18:24 -0700

    Streamline loading/saving of config

--- src/DailyData/__init__.py
+++ src/DailyData/__init__.py
@@ -3,12 +3,6 @@ import json
 
 from sys import argv as __argv
 
-from .config import Configuration
+from .config import load_config
 
-if resources.is_resource(__name__, 'config.json'):
-    # TODO change to importlib instead of pkg_resources
-    config = Configuration(
-        **json.loads(resources.read_text(__name__, 'config.json')))
-else:
-    # Create default configuration if one doesn't exist
-    config = Configuration()
+config = load_config()
--- src/DailyData/config.py
+++ src/DailyData/config.py
@@ -5,6 +5,12 @@ from . import analyzer as analyzer_pkg
 from . import time_management as time_management_pkg
 from . import tracker as tracker_pkg
 
+from importlib import resources
+import json
+import os
+
+cfg_file_location = None
+
 
 @dataclass
 class Configuration:
@@ -30,3 +36,36 @@ class Configuration:
             self.tracker = tracker_pkg.Configuration(
                 data_folder=self.data_folder,
                 **self.tracker)
+
+
+def load_config(cfg_file: Path = None):
+    if cfg_file is not None:
+        cfg_file_location = cfg_file
+        return Configuration(**json.load(cfg_file))
+
+    cwd_config = Path(os.getcwd()).joinpath('config.json')
+    usr_config = Path.home().joinpath('.dailydata_config.json')
+
+    # Try to find a configuration file. First check the directory where the script is, then check the working directory, then the user directory
+    # Along with creating the configuration, also set the save location for the config file when the program exits
+    if resources.is_resource('DailyData', 'config.json'):
+        cfg_file_location = None
+        return Configuration(**json.loads(resources.read_text(__name__, 'config.json')))
+
+    elif cwd_config.exists():
+        cfg_file_location = cwd_config
+        return Configuration(**json.load(cwd_config.open()))
+    elif usr_config.exists():
+        cfg_file_location = usr_config
+        return Configuration(**json.load(usr_config.open()))
+    else:
+        # Create default configuration if one doesn't exist
+        cfg_file_location = cwd_config
+        return Configuration()
+
+
+def save_config(config: Configuration, save_location: Path = None):
+    if save_location is not None:
+        json.dump(config, save_location.open())
+    else:
+        json.dump(config, cfg_file_location.open())
